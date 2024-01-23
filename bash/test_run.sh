d5185b356ff2b1321653b623c72e3374f42898d1
moloney89 <moloneyjack76@gmail.com>
2024-01-23 17:03:16 +0000

    Proof of concept complete, test_run.sh script created to show proof

---
+++ bash/test_run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# clear existing test dir
+rm -r ./Tester
+
+# unzip clean copy of test dir
+unzip "Tester.zip"
+
+# show old
+echo "Start State:"
+tree Tester
+
+echo "Organising..."
+python3 ./python/modules/organise_by_type.py
+ 
+echo "Complete."
+tree Tester
+
+
--- python/modules/organise_by_type.py
+++ python/modules/organise_by_type.py
@@ -1,12 +1,12 @@
 # script name: sort.py
 import os
 import shutil
-FILEPATH_TEST = '/User/jackmoloney/Developer/cs3305_team11/Tester'
+FILEPATH_TEST = '/Users/jackmoloney/Developer/cs3305_team11/Tester'
 
-def main(path_to_organise):
+def main_func(path_to_organise: str):
     file_paths = get_file_paths(path_to_organise) # array of file paths
-    file_obj_array: [File] = []
-    directories_to_create = set()
+    file_obj_array: list[File] = []
+    directories_to_create: set[str] = set()
     for file in file_paths:
         # get filetype
         filetype = determine_filetype(file)
@@ -17,13 +17,13 @@ def main(path_to_organise):
         file_obj_array.append(temp)
 
     # Folder names are defined here, pending later feature addition
-    folder_names = {'documents': 'Documents', 'photos': 'Photos', 'videos':'Videos', 'music':'Music'}
+    folder_names = {'documents': 'Documents', 'photos': 'Photos', 'videos':'Videos', 'music':'Music', 'misc':'Misc'}
     create_target_directories(path_to_organise, list(directories_to_create), folder_names) # create necessary directories
     num_files_moved = 0
     # move files to appropriate target
     for file_obj in file_obj_array:
         source = file_obj.path
-        destination = path_to_organise + folder_names[file.type]
+        destination = path_to_organise+ '/' + folder_names[file_obj.filetype]
         move_file(source, destination)
         num_files_moved += 1
     print(f'Moved {num_files_moved} files')
@@ -32,7 +32,7 @@ def main(path_to_organise):
 
 def move_file(source: str, dest: str):
     try:
-        shutil.move(source, dest, )
+        shutil.move(source, dest)
     except IOError as e:
         print(f"Error: {e}")
 
@@ -75,9 +75,10 @@ def create_target_directories(filepath: str, required: list[str], folder_names: 
     required: list[str]: list of the required folders
     """
     contents = list_dir(filepath) # return [str] of files/directories
-    for dir in required:
-        if dir not in contents:
-            os.makedirs(filepath+'/'+dir)
+    for i in required:
+        directory = folder_names[i] 
+        if directory not in contents:
+            os.makedirs(filepath+'/'+directory)
     
 
 def get_pwd():
@@ -89,15 +90,22 @@ def list_dir(filepath: str) -> str:
 
 class File:
     def __init__(self, path, filetype):
-        self.path = path
-        self.filetype = filetype
+        self._path = path
+        self._filetype = filetype
     
     @property
-    def filetype(self):
-        return self.filetype
+    def filetype(self) -> str:
+        return self._filetype
+    @filetype.setter
+    def filetype(self, f):
+        self._filetype = f
     @property
-    def path(self):
-        return self.path
+    def path(self) -> str:
+        return self._path
+    @path.setter
+    def path(self, p):
+        self._path = p
+    
     
 if __name__ == "__main__":
-    main(FILEPATH_TEST)
+    main_func(FILEPATH_TEST)
