18738eb43aa9e12ef3458334f750628a23bee042
moloney89 <moloneyjack76@gmail.com>
2024-01-25 15:23:48 +0000

    Fixed test file importing using absolute path imports.

---
+++ python/tests/test_helper_funcs.py
@@ -0,0 +1,11 @@
+import unittest
+from python.modules.helper_funcs import save_to_json
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
