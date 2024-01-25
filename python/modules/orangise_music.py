6d0ef66154a0e81e437d92d5cc7020a8764c0695
evelyn <121328261@umail.ucc.ie>
2024-01-25 15:47:49 +0000

    initial organise music commit

--- python/modules/orangise_music.py
+++ python/modules/orangise_music.py
@@ -1,13 +1,29 @@
-# from __future__ import annotations
-# import os
-# from helper_funcs import time_convert, size_convert, get_all_file_paths, get_file_and_subdir_paths, move_file, save_to_json, File, revert_changes
+from __future__ import annotations
+import os
+from helper_funcs import time_convert, size_convert, get_all_file_paths, get_file_and_subdir_paths, move_file, save_to_json, File, revert_changes
+
 import eyed3
 
-
 def get_music_data(file):
+    
+    if audio.tag is not None:
 
-    audio = eyed3.load(file)
-    track_name = audio.tag.title
-    artist = audio.tag.artist
-    album = audio.tag.album
-    year = audio.tag.year
+        audio = eyed3.load(file)
+        track_name = audio.tag.title
+        artist = audio.tag.artist
+        album = audio.tag.album
+        year = audio.tag.year
+        runtime = audio.tag.runtime
+
+
+        print(f"Title: {track_name}")
+        print(f"Artist: {artist}")
+        print(f"Album: {album}")
+        print(f"Year: {year}")
+        print(f"Runtime: {runtime}")
+
+    else:
+        print("No metadata found in the file")    
+
+mp3_file_path = "C:/Users/evely/OneDrive/Documents/Testing files/test music/Future Islands - Before the Bridge.mp3"
+get_music_data(mp3_file_path)
