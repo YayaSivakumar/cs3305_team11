dcf216f38ab10fd0675a98985e96174a4f38dd0e
moloney89 <moloneyjack76@gmail.com>
2024-01-26 16:53:15 +0000

    implemented revert_changes.py based on revert_changes function by YayaSivakumar

---
+++ python/modules/revert_changes.py
@@ -0,0 +1,33 @@
+import os
+from helper_funcs import load_json, create_list_of_file_obj, move_file, save_to_json
+
+
+def revert_changes():
+    if os.path.exists('../../cache/file_data.json'):
+        _revert_changes('../../cache/file_data.json')
+    else:
+        print("Nothing to revert")
+
+
+def _revert_changes(json_file_path: str):
+    """
+    function to revert changes and put files back in the original location
+
+    @params
+    json_file_path: str: path to JSON file containing original structure
+    """
+    with open(json_file_path, 'r') as json_file:
+        file_dict = load_json(json_file)
+
+    # create list of file objects from dictionary
+    file_list = create_list_of_file_obj(file_dict)
+
+    for file in file_list:
+        original_path = file.original_path
+        current_path = file.new_path
+        if os.path.exists(current_path) and not os.path.exists(original_path):
+            move_file(current_path, original_path)
+
+        file.original_path, file.new_path = file.new_path, file.original_path
+
+    save_to_json(file_list)
