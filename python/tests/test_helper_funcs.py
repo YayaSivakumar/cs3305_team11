3bf45611ecbf1dcd3709c157eae02fab5f9fba31
moloney89 <moloneyjack76@gmail.com>
2024-01-26 15:13:53 +0000

    Created TestHelperFuncs file and implemented unittest for save_to_json function

--- python/tests/test_helper_funcs.py
+++ python/tests/test_helper_funcs.py
@@ -1,10 +1,17 @@
 import unittest
-from python.modules.helper_funcs import save_to_json
+from python.modules.helper_funcs import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestHelperFuncs(unittest.TestCase):
+    def test_save_to_json(self):
+        test_file = File('/path/to/file', 'filetype')
+        save_to_json([test_file], 'test.json')
+        loaded = load_json('test.json')
+        print(loaded)
+        test_str = {'/path/to/file': {'original_path': '/path/to/file', 'new_path': '', 'filetype': 'filetype',
+                                      'size': '', 'creation_time': '', 'modification_time': '', 'last_access_time': ''}}
+
+        self.assertEqual(loaded, test_str)
 
 
 if __name__ == '__main__':
