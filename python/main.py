09cefedf386876835c50c15f98bfd9ffae4d523f
Daniel Cagney <121707785@umail.ucc.ie>
2024-01-30 23:19:27 +0000

    Update variable names for correct convention. Added docstring for main method for the application event loop.

--- python/main.py
+++ python/main.py
@@ -2,13 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from python.mainwindow import MainWindow
 
-# Main Application (main.py):
-# This script initializes and displays the main window.
-# It's responsible for setting up the QApplication and starting the event loop.
 def main():
+    '''Main application event loop. This script initializes and displays the main window.
+    It's responsible for setting up the QApplication and starting the event loop.'''
     app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-    mainWindow.show()
+    main_window: MainWindow = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
 
 
