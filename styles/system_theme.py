faad956f253d1d4ad48c01410b78cef7e48be4da
Conor Shipsey <cpshipsey@gmail.com>
2024-02-04 21:44:15 +0000

    Tweak to make sys theme light mode work with system's setting. Issue was that type colour would change and wouldn't be legible in accordance with system theme style sheet

--- styles/system_theme.py
+++ styles/system_theme.py
@@ -13,8 +13,16 @@ def dark_style():
                 QPushButton {
                     background-color: #5e5e5e;
                     color: #fff;
-                    border: 1px solid #2e2e2e;
+                    border: 2px solid #2e2e2e;
+                    border-radius: 10px;
+                    padding: 10px;
+                    min-width: 80px;
                 }
+                QPushButton:hover {
+                    border-color: #4f4f4f;
+                    background-color: #6e6e6e;
+                }
+
                 QMessageBox {
                     background-color: #393939;
                     color: #fff;
@@ -27,12 +35,22 @@ def light_style():
             QMainWindow {
                 background-color: #F5F5F5;
             }
+            QTreeView {
+                    color: #000;
+            }
+            QLabel {
+                    color: #000;
+            }
+            QMessageBox {
+                    color: #000;
+            }
             QPushButton {
                 border: 2px solid #8F8F8F;
                 border-radius: 10px;
                 background-color: #E0E0E0;
                 padding: 10px;
                 min-width: 80px;
+                color: #000;
             }
             QPushButton:hover {
                 border-color: #3daee9;
