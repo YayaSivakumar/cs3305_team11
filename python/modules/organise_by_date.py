e91358f3b3653ba5c5f35eae0863706d72067e1c
yachitrasivakumar <yaya.s@hotmail.co.uk>
2024-02-10 19:37:27 +0000

    migrated organise_by_date.py to use new model

--- python/modules/organise_by_date.py
+++ python/modules/organise_by_date.py
@@ -1,10 +1,8 @@
 # script name: organise_by_date.py
-from __future__ import annotations
 import os
-from python.modules.helper_funcs import time_convert, size_convert, get_all_file_paths, get_file_and_subdir_paths, move_file, save_to_json, File
+from python.model.FileSystemNodeModel import Directory, FileSystemCache
 
-
-def organise_by_date_func(path_to_organise: str, dir_traversal_type: function = get_all_file_paths):
+def organise_by_date(dir_node: Directory):
     """
     main function for organise by date function.
 
@@ -13,62 +11,11 @@ def organise_by_date_func(path_to_organise: str, dir_traversal_type: function = 
     dir_traversal_type: function: name of function to be used to traverse the directory
     """
 
-    # get metadata from each file in the directory
-    md_list = get_metadata_from_files(dir_traversal_type(path_to_organise))
     # create the required directories and move the files
-    organise_by_date(md_list, path_to_organise) 
-
-    # Save the original structure to a JSON file
-    save_to_json(md_list)
+    _organise_by_date(dir_node)
 
 
-def get_item_paths(path: str):
-    """
-    Function to get the paths of all files and immediate subdirectories in a directory.
-
-    @params
-    path: str: path to root directory
-
-    returns
-    contents_list: list: list of paths to all files and immediate subdirectories
-    """
-    files_list = []
-    # List all entries in the given directory
-    for entry in os.listdir(path):
-        full_path = os.path.join(path, entry)
-        # Check if the entry is a file or a directory (but don't recurse into subdirectories)
-        if os.path.isfile(full_path) or os.path.isdir(full_path):
-            files_list.append(full_path)
-    
-    return files_list
-
-
-def get_metadata_from_files(filepath_list: list):
-    """
-    function to get the metadata from a file 
-
-    @params
-    filepath: str: absolute path to a file
-    """
-    md_list = []
-
-    # get the time data from each file
-    for filepath in filepath_list:
-
-        stats = os.stat(filepath)
-        
-        file = File(filepath, filepath.split('.')[-1])
-        file.size = size_convert(stats.st_size)
-        file.creation_time = time_convert(stats.st_ctime)
-        file.modification_time = time_convert(stats.st_mtime)
-        file.last_access_time = time_convert(stats.st_atime)
-        
-        md_list.append(file)
-
-    return md_list
-
-
-def organise_by_date(md_list: list, directory_path: str):
+def _organise_by_date(dir_node: Directory):
     """
     function to create the required directories
 
@@ -78,29 +25,42 @@ def organise_by_date(md_list: list, directory_path: str):
     """
     month_names = {'01':'January', '02':'February', '03':'March', '04':'April', '05':'May', '06':'June',\
                    '07':'July', '08':'August', '09':'September', '10':'October', '11':'November', '12':'December'}
-    
-    # iterate through each file and obtain year and month of modification(/creation/last access)
-    for file in md_list:
-        # get list of directories already created in path
-        list_of_directories = os.listdir(directory_path)
-        # get year and month of modification
-        year = file.creation_time.split('-')[0]
-        m = file.creation_time.split('-')[1]
-     
-        month = month_names[m]  # for test purposes I am using the 'Modified Time' values
 
-        # create directories if they don't already exist
-        if year not in list_of_directories:
-            os.makedirs(directory_path+'/'+year)
-        if month not in os.listdir(directory_path+'/'+year):
-            os.makedirs(directory_path+'/'+year+'/'+month)
+    # create copy of child files to avoid changing the list while iterating
+    children = [child for child in dir_node.children]
 
-        # move item to the correct directory
-        move_file(file.original_path, directory_path+'/'+year+'/'+month)
+    for node in children:
 
-        # update new path in metadata dictionary
-        file.current_path = directory_path+'/'+year+'/'+month+'/'+(file.original_path.split('/'))[-1]
+        if not node.is_invisible():
+
+            # extract year and month from modification date
+            year = str(node.modification_date()).split('-')[0]
+            month = month_names[str(node.modification_date()).split('-')[1]]
+
+            # create directories if they don't already exist
+            if year not in os.listdir(dir_node.path):
+                os.makedirs(dir_node.path+'/'+year)
+                dir_node.add_child(Directory(dir_node.path+'/'+year, dir_node.cache))
+            if month not in os.listdir(dir_node.path+'/'+year):
+                os.makedirs(dir_node.path+'/'+year+'/'+month)
+                year_node = dir_node.find_node(year)
+                year_node.add_child(Directory(dir_node.path + '/' + year + '/' + month, dir_node.cache))
+
+            # move item to the correct directory
+            new_file_path = os.path.join(dir_node.path+'/'+year+'/'+month + '/' + node.name())
+            node.move(new_file_path)
 
 
 if __name__ == "__main__":
-    pass
+    # testing
+    '''
+    cache = FileSystemCache()
+    dir_obj = Directory('/Users/yachitrasivakumar/Desktop/test_by_date', cache)
+    print(dir_obj.list_contents())
+    organise_by_date(dir_obj)
+    print(dir_obj.list_contents())
+    twennytwennyfour = dir_obj.find_node('2024')
+    print(twennytwennyfour.list_contents())
+    twennytwennyfourjan = twennytwennyfour.find_node('January')
+    print(twennytwennyfourjan.list_contents())
+    '''
