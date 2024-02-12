8211eb742bddf89ea283c084cb8a0a41f487d9be
Conor Shipsey <cpshipsey@gmail.com>
2024-02-12 17:02:06 +0000

    Have widget disappear on click

--- python/ui/toast_message.py
+++ python/ui/toast_message.py
@@ -1,55 +1,30 @@
 # toast_message.py
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton
-from python.styles.toast import ToastStyle, SuccessStyle, AlertStyle, ErrorStyle, InfoStyle
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton, QGridLayout
+from python.styles.toast import SuccessStyle, AlertStyle, ErrorStyle, InfoStyle
 
 
-class ToastMessage(QWidget):
+class ToastMessage(QPushButton):
     def __init__(self, message, status, timeout=True, opacity=1.0, font_size=12, parent=None):
-        super(ToastMessage, self).__init__(parent)
-
-        self.label = QLabel(message)
-        self.label.setAlignment(Qt.AlignCenter)
-
-        self.timeout = timeout
+        super(ToastMessage, self).__init__(message, parent)
 
         # Select the appropriate style based on the status
         style_classes = {
-            'success': SuccessStyle(opacity=opacity, font_size=font_size),
-            'alert': AlertStyle(opacity=opacity, font_size=font_size),
-            'error': ErrorStyle(opacity=opacity, font_size=font_size),
+            'success': SuccessStyle(),
+            'alert': AlertStyle(),
+            'error': ErrorStyle(),
         }
-        self.selected_style_class = style_classes.get(status, InfoStyle(opacity=opacity, font_size=font_size))
+        self.selected_style_class = style_classes.get(status, InfoStyle())
 
         # Instantiate the style class and get the styles
         self.style = self.selected_style_class.get_main_style() + self.selected_style_class.get_style()
-
-        layout = QVBoxLayout(self)
-        self.label = self.create_label(message)
-        layout.addWidget(self.label)
         self.setStyleSheet(self.style)
 
-        if not timeout:
-            close_button = self.create_close_button()
-            layout.addWidget(close_button)
-
-        if timeout:
-            QTimer.singleShot(3000, self.close)  # Close the toast after 3 seconds
-
-        self.setGeometry(400, 400, 300, 100)  # Adjust the size and position as needed
-
-    def create_close_button(self):
-        close_button = QPushButton("x", self)
-        close_button.clicked.connect(self.close)
-        close_button.setStyleSheet("color: #000000;")
-        return close_button
-
-    def create_label(self, message):
-        label = QLabel(message, self)
-        label.setAlignment(Qt.AlignCenter)
-        label.setStyleSheet("color: #000000;")
-        return label
+        self.setGeometry(400, 400, 200, 70)  # Adjust the size and position as needed
+        self.clicked.connect(self.hide)
 
     def show_toast(self):
         self.show()
         self.raise_()  # Bring the toast window to the front
+
+
