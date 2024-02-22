b4b09795b96c853eda3a74608d89f979e49e899e
Daniel Cagney <121707785@umail.ucc.ie>
2024-02-22 21:44:41 +0000

    Updated font size, and font family for consistency

--- styles/sidebar.py
+++ styles/sidebar.py
@@ -1,4 +1,5 @@
 def main_style():
+
     return """
             QWidget {
                 border: 1px solid #d0d0d0;  /* Grey border */
@@ -6,7 +7,10 @@ def main_style():
                 padding: 10px;  /* Padding inside the sidebar */
                 color: black; /* White text */
             }
+    
             QPushButton {
                 margin: 5px;  /* Space between buttons */
+                font-family: "Arial", sans-serif;  /* Font */
+                font-size: 16px;  /* Font size */
             }
         """
