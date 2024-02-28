0a896bc7962bf33cc368663881c7481c6900bdf2
Conor Shipsey <cpshipsey@gmail.com>
2024-02-28 16:39:35 +0000

    Changed lazy to dynamic

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -5,12 +5,13 @@ from bcrypt import hashpw, gensalt, checkpw
 
 from .files_uploaded import UploadedFiles
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(24))
     email = db.Column(db.String(44), unique=True)
     password_hashed = db.Column(db.String(128))
-    files = db.relationship('File', secondary=UploadedFiles, backref=db.backref('user', lazy=True))
+    files = db.relationship('File', secondary=UploadedFiles, backref=db.backref('user', lazy='dynamic'))
 
     def __repr__(self):
         return f"<User {self.username}>"
