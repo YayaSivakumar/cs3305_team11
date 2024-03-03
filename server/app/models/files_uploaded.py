a5a5a3d56c601b25d700714def048da4a2809450
Conor Shipsey <cpshipsey@gmail.com>
2024-03-03 18:01:35 +0000

    Working on db models to work on 1-M relationship between user and files uploaded

--- server/app/models/files_uploaded.py
+++ server/app/models/files_uploaded.py
@@ -1,6 +1,6 @@
-from ..db import db
-
-UploadedFiles = db.Table('uploaded_files',
-                         db.Column('file_id', db.Integer, db.ForeignKey('file.id'), primary_key=True),
-                         db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-                         )
+# from ..db import db
+#
+# UploadedFiles = db.Table('uploaded_files',
+#                          db.Column('file_id', db.Integer, db.ForeignKey('file.id'), primary_key=True),
+#                          db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+#                          )
