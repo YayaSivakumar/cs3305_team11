03820ff013b67b18b529345a28e28fb7af6a26de
Conor Shipsey <cpshipsey@gmail.com>
2024-03-08 20:12:45 +0000

    documenting

--- server/app/app.py
+++ server/app/app.py
@@ -6,47 +6,55 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from .db import db
 from .login_manager import login_manager
 
-from .models.file import File
+from .models.file_upload import File
 from .models.user import User
 
 from .routes import register_routes
-from .routes import main_routes as m, file_routes as f, user_routes as u
-from flask_login import LoginManager
 
 from .config import UPLOADS_FOLDER
 from .config import SECRET_KEY
 
 
 def create_app():
+    """
+    Create a Flask app and return it
+    """
     app = Flask(__name__)
+    # Set the app configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['UPLOAD_FOLDER'] = UPLOADS_FOLDER
     app.config['SECRET_KEY'] = SECRET_KEY
-
+    # Initialize the app with the database
     db.init_app(app)
     with app.app_context():
+        # Create the database tables
         db.create_all()
-
+    # Initialize the flask login manager
     login_manager.init_app(app)
     login_manager.login_view = 'user_routes.login'
 
     @login_manager.user_loader  # Add user_loader callback here
     def load_user(user_id):
+        # This callback is used to reload the user object from the user ID stored in the session
         return User.query.get(int(user_id))
 
-    register_routes(app)
-    # app.register_blueprint(m.main_routes)
-    # app.register_blueprint(f.file_routes)
-    # app.register_blueprint(u.user_routes)
+    # Register the all the routes' blueprints
+    register_routes(app)  # Register routes here
 
     return app
 
 
 def cleanup_expired_files(app):
+    """
+    Remove expired files from the database and the file system
+    """
     with app.app_context():
+        # Remove expired files from the database and the file system
         now = datetime.utcnow()
+        # Get all the expired files
         expired_files = File.query.filter(File.expires_at <= now).all()
         for file in expired_files:
+            # Remove the file from the file system
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.unique_id)
             if os.path.exists(filepath):
                 os.remove(filepath)
@@ -55,6 +63,10 @@ def cleanup_expired_files(app):
 
 
 def start_scheduler(app):
+    """
+    Start the scheduler to cleanup expired files
+    """
     scheduler = BackgroundScheduler()
+    # Schedule the cleanup_expired_files function to run every hour
     scheduler.add_job(func=lambda: cleanup_expired_files(app), trigger="interval", hours=1)
     scheduler.start()
--- server/app/config.py
+++ server/app/config.py
@@ -1,4 +1,8 @@
 import os
 
+"""
+This file contains the configuration for the Flask app
+"""
+
 UPLOADS_FOLDER = os.getenv('UPLOADS_FOLDER')
 SECRET_KEY = os.getenv('SECRET_KEY')
