c0a72f80ef1c4eac8e1eb4b5fcc140503f66dfd7
Conor Shipsey <cpshipsey@gmail.com>
2024-03-08 20:15:04 +0000

    Documenting

--- server/app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from server.run import app
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
--- server/run.py
+++ server/run.py
@@ -1,5 +1,9 @@
 from app.app import create_app, start_scheduler
 
+"""
+This is the entry point of the application.
+It will create the app instance and start the scheduler.
+"""
 
 if __name__ == '__main__':
     app_instance = create_app()
