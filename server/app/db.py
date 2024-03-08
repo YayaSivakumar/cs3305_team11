49ef4a80f6ebb55063c902164281cbe65a670b6c
Conor Shipsey <cpshipsey@gmail.com>
2024-03-08 20:13:37 +0000

    Documenting

--- server/app/db.py
+++ server/app/db.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
+"""
+This file is used to create a SQLAlchemy object that will be used to interact with the database.
+"""
+
 db = SQLAlchemy()
--- server/app/login_manager.py
+++ server/app/login_manager.py
@@ -1,3 +1,7 @@
 import flask_login
 
+"""
+This file is used to create a login_manager object that will be used to manage user logins.
+"""
+
 login_manager = flask_login.LoginManager()
